7ccc71035580418284ec854237f2e1e62540474d
rajath-dev <rajathdev12@gmail.com>
2022-11-18 12:29:39 +0530

    auth page creation

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const dashboard: React.FC = () => {
-  return <div>dashboard</div>;
+const Dashboard: React.FC = () => {
+  return <div>Dashboard</div>;
 };
 
-export default dashboard;
+export default Dashboard;
